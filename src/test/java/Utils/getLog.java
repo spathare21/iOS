e95d79938739c8410d47a8643db78e6583f01f37
Sachin Pathare <sachin.pathare@vertisinfotech.com>
2016-05-12 15:43:33 +0530

    added code in gitlod to read device info

--- src/test/java/Utils/getLog.java
+++ src/test/java/Utils/getLog.java
@@ -9,9 +9,9 @@ import java.io.*;
 public class getLog {
 
     public static void getlog(String udid) throws IOException {
-        System.out.println("execting command for system log");
+        System.out.println("executing command for system log");
         System.out.println("in getlog udid is : " + udid);
-        String command = "idevicesyslog -u " + udid + " >> system.log";
+        final String command = "idevicesyslog -u " + udid + " >> system.log";
 
         System.out.println("command is " + command);
 
@@ -32,4 +32,93 @@ public class getLog {
         Process pr = run.exec(final_command);
 
     }
+
+    public static String getUdid() throws IOException {
+
+        String deviceid = "";
+
+        System.out.println("executing command for getting Device udid");
+        final String command1 = "idevice_id --list";
+
+        System.out.println("command is " + command1);
+
+        String[] final_command1 = CommandLine.command(command1);
+        System.out.println("final command to get udid is " + final_command1);
+
+        Runtime run = Runtime.getRuntime();
+        Process pr = run.exec(final_command1);
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(pr.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(pr.getErrorStream()));
+
+        // read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String deviceudid = null;
+        while ((deviceudid = stdInput.readLine()) != null) {
+            System.out.println(" Device Id  :" + deviceudid + "\n");
+            deviceid = deviceudid;
+
+        }
+        // read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((deviceudid = stdError.readLine()) != null) {
+            System.out.println("Some error occured while executing command ");
+        }
+        return deviceid;
+    }
+
+
+    public static String getDeviceInfo() throws IOException {
+
+        String deviceInfo = " ";
+        String DeviceOs = " " ;
+        String DeviceName = " ";
+
+        System.out.println("executing command for getting Device Info");
+        final String command2 = "ideviceinfo";
+
+        System.out.println("command is " + command2);
+
+        String[] final_command2 = CommandLine.command(command2);
+        System.out.println("final command to get Device Info " + final_command2);
+
+        Runtime run = Runtime.getRuntime();
+        Process pr = run.exec(final_command2);
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(pr.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(pr.getErrorStream()));
+
+        // read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String deviceinfo = null;
+        while ((deviceinfo = stdInput.readLine()) != null) {
+
+            if (deviceinfo.contains("DeviceName"))
+            {
+                DeviceName = deviceinfo;
+                System.out.println("" + DeviceName + "\n");
+            }
+
+            if(deviceinfo.contains("ProductVersion"))
+            {
+                DeviceOs = deviceinfo;
+                System.out.println("Device Os is :" + DeviceOs + "\n");
+            }
+            
+        }
+        // read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((deviceinfo = stdError.readLine()) != null) {
+            System.out.println("error occured while executing command ");
+        }
+        return (DeviceName + " " + DeviceOs);
+    }
+
+
 }
