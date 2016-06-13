a1889b93bba9871dbdc0b120485ed9445d765ea2
shivam gupta <shiam.gupta@vertisinfotech.com>
2016-06-13 19:25:46 +0530

    AdvancedPlayback Completed

---
+++ src/test/java/pageObject/AdvancedplaybackSampleApp.java
@@ -0,0 +1,26 @@
+package pageObject;
+
+
+
+import org.openqa.selenium.WebElement;
+import io.appium.java_client.AppiumDriver;
+
+
+/**
+ * Created by Shivam on 13/06/16.
+ */
+public class AdvancedplaybackSampleApp {
+
+    public static void select_asset(AppiumDriver driver) {
+        System.out.println("Selecting Video 2");
+        java.util.List<WebElement> l = driver.findElementsByClassName("UIAButton");
+        l.get(5).click();
+
+    }
+
+    public static void custom_Play_pause(AppiumDriver driver)
+    {
+        java.util.List<WebElement> l = driver.findElementsByClassName("UIAButton");
+        l.get(2).click();
+}
+}
